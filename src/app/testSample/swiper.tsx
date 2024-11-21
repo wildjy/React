fa94e6a102e72e63cd488ca93be684ac5eb21039
yu jae yeong <luck5252@nate.com>
2024-11-21 23:26:37 +0900

    swiper 해상도 조건별 렌더링

--- src/app/testSample/swiper.tsx
+++ src/app/testSample/swiper.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
@@ -11,11 +11,35 @@ interface SwiperComponentProps {
 }
 
 const SwiperComponent: React.FC<SwiperComponentProps> = ({ slides }) => {
+  const [isNavigationEnabled, setIsNavigationEnabled] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+
+      if (width < 1024) {
+        setIsNavigationEnabled(true); // 768px 이상, 1024px 미만에서는 활성화
+      } else {
+        setIsNavigationEnabled(false); // 그 외에는 비활성화
+      }
+    };
+
+    // 초기 설정
+    handleResize();
+
+    // 리사이즈 이벤트 리스너 추가
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      // 리스너 제거
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <>
       <Swiper
-        modules={[Navigation, Pagination]}
+        modules={[Navigation, Pagination, ...(isNavigationEnabled ? [Navigation] : [])]}
         spaceBetween={0}
         slidesPerView= "auto" // 슬라이드 너비가 CSS로 지정됨
         loop={false}
@@ -23,7 +47,7 @@ const SwiperComponent: React.FC<SwiperComponentProps> = ({ slides }) => {
           delay: 2500,
           disableOnInteraction: false,
         }}
-        navigation={true}
+        navigation={isNavigationEnabled} // 상태에 따라 navigation 활성화
         pagination={{ clickable: true }}
         // onSlideChange={() => console.log('슬라이드가 변경되었습니다!')}
         // onSwiper={(swiper) => console.log('Swiper 인스턴스:', swiper)}
