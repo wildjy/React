04cdc9f8444d1466b0fd4954a1ac450097788719
yu jae young <lucky0914@jinhak.com>
2025-05-08 18:05:46 +0900

    좌측 aside + scroll fixed layout
    contents min-height 수정

--- src/sharedUI/Tong/TongContents.tsx
+++ src/sharedUI/Tong/TongContents.tsx
@@ -55,65 +55,63 @@ function ScrollPageContents({ addClass, children }: { sideNav?: boolean; addClas
   }, [setThreshold]);
 
   return (
-    <div className={`3xl:min-h-[62.5rem]`}>
-      <div className="flex items-stretch">
-        {slots.length > 0 ? (
-          slots.slice(0, 2).map((slot, index) =>
-            index === 0 ? (
-              sideNav && (
-                <ScrollFixed key={index} top={`top-[0] left-auto right-auto z-[5] h-full`}>
-                  <div ref={targetRef} className={`${cn('scroll contents....  h-full relative', addClass)}`}>
-                    {/* ${sideNav && !isDesktop ? 'absolute' : 'fixed'} */}
-                    <div
-                      className={`
-                        ${cn(
-                          [
-                            `
-                            fixed 3xl:absolute
-                            top-0 bottom-0 w-[11.625rem]
-                            border-r border-gray-200 bg-white
-                            transition duration-150 z-[10] 3xl:z-[5]`,
-                          ],
-                          {
-                            '3xl:top-[4.375rem]': isFixed && topBottom,
-                            '-translate-x-full': isDesktop && !isNavOpen,
-                            'translate-x-0': !isDesktop || isNavOpen,
-                            hidden: isDesktop === undefined,
-                          }
-                        )}
-                      `}
-                    >
-                      {isDesktop && (
-                        <div
-                          className={`
-                          absolute -right-[2.5rem] -translate-y-1/2 top-1/2
-                          flex justify-center items-center
-                          w-[2.5rem] h-[2.5rem]
-                          text-white bg-gray-1000
-                        `}
-                        >
-                          <button onClick={() => setIsNavOpen(!isNavOpen)}>{isNavOpen ? '열고' : '닫기'}</button>
-                        </div>
+    <div className="flex items-stretch min-h-[62.5rem]">
+      {slots.length > 0 ? (
+        slots.slice(0, 2).map((slot, index) =>
+          index === 0 ? (
+            sideNav && (
+              <ScrollFixed key={index} top={`top-[0] left-auto right-auto z-[5] h-full`}>
+                <div ref={targetRef} className={`${cn('scroll contents....  h-full relative', addClass)}`}>
+                  {/* ${sideNav && !isDesktop ? 'absolute' : 'fixed'} */}
+                  <div
+                    className={`
+                      ${cn(
+                        [
+                          `
+                          fixed 3xl:absolute
+                          top-0 bottom-0 w-[11.625rem]
+                          border-r border-gray-200 bg-white
+                          transition duration-150 z-[10] 3xl:z-[5]`,
+                        ],
+                        {
+                          '3xl:top-[4.375rem]': isFixed && topBottom,
+                          '-translate-x-full': isDesktop && !isNavOpen,
+                          'translate-x-0': !isDesktop || isNavOpen,
+                          hidden: isDesktop === undefined,
+                        }
                       )}
-                      {slot}
-                    </div>
+                    `}
+                  >
+                    {isDesktop && (
+                      <div
+                        className={`
+                        absolute -right-[2.5rem] -translate-y-1/2 top-1/2
+                        flex justify-center items-center
+                        w-[2.5rem] h-[2.5rem]
+                        text-white bg-gray-1000
+                      `}
+                      >
+                        <button onClick={() => setIsNavOpen(!isNavOpen)}>{isNavOpen ? '열고' : '닫기'}</button>
+                      </div>
+                    )}
+                    {slot}
                   </div>
-                </ScrollFixed>
-              )
-            ) : (
-              <div key={index} className={`3xl:px-[17.375rem] grow`}>
-                <div className={`${innerClass}`}>{slot}</div>
-              </div>
+                </div>
+              </ScrollFixed>
             )
-          )
-        ) : (
-          <div className={`3xl:px-[17.375rem] grow`}>
-            <div className={`${innerClass}`}>
-              <NoData message="No Contents." />
+          ) : (
+            <div key={index} className={`3xl:px-[17.375rem] grow`}>
+              <div className={`${innerClass}`}>{slot}</div>
             </div>
+          )
+        )
+      ) : (
+        <div className={`3xl:px-[17.375rem] grow`}>
+          <div className={`${innerClass}`}>
+            <NoData message="No Contents." />
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
