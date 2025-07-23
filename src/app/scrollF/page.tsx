a1763694bdbb6f83bca5ed0837cee1c99f42fe26
yu jae young <lucky0914@jinhak.com>
2025-07-23 18:19:26 +0900

    scroll floating 컴포넌트

--- src/app/scrollF/page.tsx
+++ src/app/scrollF/page.tsx
@@ -43,29 +43,48 @@ function ScrollPageContents() {
   }, [setThreshold]);
 
   return (
-    <div className="flex h-full flex-col">
+    <ScrollProvider>
+      <div className="flex w-full h-full flex-col">
 
-      <ScrollFixed top={`top-0`} fixHeight={'h-[75px]'}>
+        {/* <ScrollFixed top={`top-0`} fixHeight={'h-[75px]'}>
+          <div className="flex justify-center items-center w-full h-[75px]">
+            header {isFixed ? 'fix' : 'not fix'}
+          </div>
+        </ScrollFixed> */}
+
         <div className="flex justify-center items-center w-full h-[75px]">
           header {isFixed ? 'fix' : 'not fix'}
         </div>
-      </ScrollFixed>
 
-      <div className="h-[9.375rem] bg-gray-500"></div>
-      <div className="absolute top-[9.375rem] left-0 w-[10px] h-[1px] bg-red-600 z-50"></div>
+        <div className="h-[9.375rem] bg-gray-500"></div>
+        <div className="absolute top-[9.375rem] left-0 w-[10px] h-[1px] bg-red-600 z-50"></div>
 
-      <div className="flex flex-grow flex-col bg-green-100">
-        <div className='h-[62.5rem]'></div>
+        <div className="flex flex-grow flex-col bg-green-100">
+          <div className='h-[62.5rem]'></div>
+        </div>
+
+        <div ref={targetRef} className="sm:pb-17 md:pb-18 xl:pb-0">
+          <div className='h-[15.7rem] md:h-[12rem] xl:h-[14.1875rem] bg-gray-700'></div>
+        </div>
+
+        {/* <ScrollBottom>  targetH={targetTop}
+          <nav className="px-5 py-3">{scrollDirection ? 'scroll Down' : 'scroll Up'}</nav>
+        </ScrollBottom> */}
+
+        <ScrollFloating direction align='left'>
+          <div className='flex flex-col gap-3'>
+            <button className='bg-red-500 rounded-full size-9 md:size-12 xl:size-11'></button>
+          </div>
+        </ScrollFloating>
+
+        <ScrollFloating >
+          <div className='flex flex-col gap-3'>
+            <button className='w-10 h-10 rounded-full bg-red-200'></button>
+            <button className='w-10 h-10 rounded-full bg-black'></button>
+          </div>
+        </ScrollFloating>
+
       </div>
-
-      <div ref={targetRef} className="py-[4.5rem] bg-gray-700">footer</div>
-
-      <ScrollBottom>
-        <nav className="px-5 py-3">{scrollDirection ? 'scroll Down' : 'scroll Up'}</nav>
-      </ScrollBottom>
-
-      <ScrollFloating h={targetTop} />
-
-    </div>
+    </ScrollProvider>
   );
 }
--- src/sharedUI/Layout/ScrollFloating.tsx
+++ src/sharedUI/Layout/ScrollFloating.tsx
@@ -1,51 +1,91 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import throttle from 'lodash/throttle';
 import { useScroll } from './Provider/ScrollProvider';
 import { cn } from "../common/cn";
 
 interface ScrollFloatingProps {
   children?: React.ReactNode;
+  direction?: boolean;
+  align?: string;
   addClass?: string;
-  h: number;
+  targetF?: boolean;
 }
 
-export const ScrollFloating: React.FC<ScrollFloatingProps> = ({ children, addClass, h }) => {
+export const ScrollFloating: React.FC<ScrollFloatingProps> = ({ children, direction = false, align, addClass, targetF = false }) => {
   const { scrollDirection } = useScroll();
-  console.log(h)
-  const [footerY, setFooterY] = useState(0);
+  const [ footerY, setFooterY ] = useState(0);
+  const [ scrollTop, setScrollTop ] = useState(0);
+  const [ targetTop, setTargetTop ] = useState(0);
+  const targetRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    const handleScroll = () => {
+    const setOffsetTop = () => {
+      if (targetRef.current) {
+        const offsetTop = targetRef.current?.clientHeight;
+        setTargetTop(offsetTop) // scroll 임의 타겟 위치
+      }
+    };
+
+    setOffsetTop();
+
+    const resizeEvent = throttle(() => {
+      setOffsetTop();
+    }, 50);
+
+    window.addEventListener('resize', resizeEvent);
+    return () => {
+      window.removeEventListener('resize', resizeEvent);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = throttle(() => {
       const pageY = window.scrollY;
       const docHeight = document.documentElement.scrollHeight;
       const winHeight = window.innerHeight;
 
-      const calcFooterY = docHeight - h - winHeight - pageY;
+      setScrollTop(pageY);
+      const calcFooterY = docHeight - targetTop - winHeight - pageY;
       setFooterY(calcFooterY);
-    };
+    }, 10);
 
     window.addEventListener('scroll', handleScroll);
-    handleScroll(); // 초기 실행
+    handleScroll();
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, [h]);
+  }, [scrollTop, targetTop]);
 
   return (
     <div
-      className={`fixed bottom-0 w-full transition-[bottom] border-t inset-shadow-2xs`} //duration-300 ease-linear
+      className={`block fixed bottom-0`} // md:hidden
     >
-      <div className={`
-      ${cn('',
-        scrollDirection ? 'opacity-1 visible' : '',
-        footerY <= 0 ? 'mb-4' : 'mb-14'
-      )}
-        fixed right-4 bottom-0 z-[10] duration-100
-        `} //${scrollDirection ? 'opacity-1 visible' : 'opacity-0 invisible'}
-        style={{ bottom: footerY <= 0 ? -footerY : undefined  }}
-      >
-        <button className='w-10 h-10 rounded-full bg-black'>{footerY}</button>
+      {/* footer 기준 높이 추출용 */}
+      <div ref={targetRef} className="w-[1px] sm:pb-17 md:pb-18 xl:pb-0 bg-red-700">
+        <div className={`${align === 'left' ? 'h-[12rem]' : 'h-[15.7rem] md:h-[12rem] xl:h-[14.1875rem]'}`}></div>
+      </div>
+
+      <div>
+        <div className={`
+          ${cn('fixed mb-4 bottom-0 z-[10] transition-[background-color] duration-100',
+            align === 'left' ?
+            'left-4' :
+            'right-4 ',
+            // 'right-4 mb-[7rem] sm:mb-[7.5rem]',
+            direction && (scrollDirection ? 'opacity-1' : 'opacity-0'),
+            scrollTop ? 'block' : 'hidden',
+            targetF ? (
+              footerY <= 0 && 'mb-0' //mb-[4.5rem] sm:mb-[4rem]
+            ) : undefined
+          )}
+        `}
+
+          style={{ bottom: targetF && footerY <= 0 ? -footerY : undefined  }}
+        >
+          {children}
+        </div>
       </div>
     </div>
   );
